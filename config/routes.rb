73c58c4bfac0893826c164e74105e0b595020549
JoaoCostaWM <joao.costa@wiremaze.com>
2015-11-11 10:52:01 +0000

    basic user model including secure pw

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
   get 'users/new'
 
+  get 'users/new'
+
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
