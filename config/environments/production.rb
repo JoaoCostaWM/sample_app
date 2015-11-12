71dd9fa2e169127340d9337afe77fbf73a33342d
JoaoCostaWM <joao.costa@wiremaze.com>
2015-11-12 15:52:11 +0000

    Add PW resetes and email config(email still need attention, missing sendgrid )

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,25 @@ SampleApp::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+=begin
+HEROKU CONFIG TO SEND EMAILS IN PRODUCTION
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = '<your heroku app>.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+=end
+
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
